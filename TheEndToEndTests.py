4aa0229dd23ae27d469dcfc8f14e0063cd7b8d91
Dylan S <56564270+Dylan-SC@users.noreply.github.com>
2020-04-04 14:54:32 -0500

    Update TheEndToEndTests.py

--- TheEndToEndTests.py
+++ TheEndToEndTests.py
@@ -6,3 +6,7 @@ class Index_Home_Page_Testing(BaseCase):
 
 	#Define the tests here
 		#then define the elements to be tested here
+	
+	def Test_BMI_Calculator(self):
+    	# Check when the webpage loads if element exists
+    	#self.assert_element_not_visible('label#BMI_Result')
