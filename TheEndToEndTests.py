a0373c4fe855876614e8ebd1756381884a3eb245
Dylan S <56564270+Dylan-SC@users.noreply.github.com>
2020-04-04 14:41:53 -0500

    Update TheEndToEndTests.py

--- TheEndToEndTests.py
+++ TheEndToEndTests.py
@@ -1,1 +1,8 @@
+# This file is used to test the functionaliy of the webpage itself created via python
 
+from seleniumbase import BaseCase
+
+class Index_Home_Page_Testing(BaseCase):
+
+	#Define the tests here
+		#then define the elements to be tested here
